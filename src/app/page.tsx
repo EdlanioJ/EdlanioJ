46a3834db3a0c1f7cdd63fed7e5442e7dd668b06
Edlâneo Júlio <edlanioj@gmail.com>
2023-08-25 08:30:19 +0100

    feat: add link to github

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,21 @@
+/* eslint-disable react/no-unescaped-entities */
+
+import Link from 'next/link'
+
 export default function Home() {
   return (
     <main className="flex h-screen w-full items-center justify-center gap-1 dark:text-zinc-100">
-      <p>Hi there 👋, its </p>
-      <span className=" font-bold">EdlanioJ Portfolio</span>
+      <p>
+        Hi there 👋, it's{' '}
+        <Link
+          href="https://github.com/EdlanioJ"
+          target="_blank"
+          className="font-bold hover:underline"
+        >
+          EdlanioJ
+        </Link>{' '}
+        Portfolio
+      </p>
     </main>
   )
 }
