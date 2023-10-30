8636180278b7a7686cc18baa7f3d14690bccd8a2
Edlâneo Júlio <edlanioj@gmail.com>
2023-10-30 13:14:14 +0100

    update prettier and tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,27 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import type { Config } from 'tailwindcss'
+
+export default {
   content: ['./src/**/*.{ts,tsx}'],
   theme: {
     extend: {
       fontFamily: {
-        sans: ['var(--font-inter)'],
+        sans: ['var(--font-poppins)'],
+      },
+      colors: {
+        teal: {
+          '500': '#00ADB5',
+        },
+        orange: {
+          '500': '#ee6c4d',
+        },
+
+        gray: {
+          '50': '#EEEEEE',
+          '500': '#393E46',
+          '900': '#222831',
+        },
       },
     },
   },
   plugins: [],
-}
+} satisfies Config
